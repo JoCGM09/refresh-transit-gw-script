8faf8ff4a5e1af8f7d9d803bf823bd129393d92f
Jose Guerra <jose.guerra.m@uni.pe>
2024-04-16 11:04:20 -0500

    transit-vuelta-update

--- script-ida.sh
+++ script-ida.sh
@@ -8,6 +8,8 @@ ibmcloud login --apikey $IBM_CLOUD_API_KEY -r 'us-south' -g 'Unique' -q
 
 # 1. Denegar el pase de 172.20.19.0/29 en conexión a DAL10
 
+echo("ibmcloud tg prefix-filter-update $IBM_TRANSIT_GW_ID $IBM_TRANSIT_CC_ID_DAL10 $IBM_TRANSIT_PREFIX_ID_DAL10 --prefix 172.20.19.0/29 --action deny")
+
 ibmcloud tg prefix-filter-update $IBM_TRANSIT_GW_ID $IBM_TRANSIT_CC_ID_DAL10 $IBM_TRANSIT_PREFIX_ID_DAL10 --prefix 172.20.19.0/29 --action deny
 
 sleep 15
